38dbad7441ec469fa1bc661a966d1a5075b839b9
harafromhell <clubxmonaco@gmail.com>
2018-09-12 22:28:59 -0400

    added correct paths & comments

--- patch_v1.sh
+++ patch_v1.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+#create human readable path variables to common paths.
 PATH_TO_BOOT_ONCE='/usr/local/outset/boot-once'
-echo $PATH_TO_BOOT_EVERY
 PATH_TO_LOGIN_PRIVELEGED_ONCE='/usr/local/outset/login-privileged-once'
-echo $PATH_TO_LOGIN_PRIVELEGED_ONCE
 TEST_PATH='/tmp'
-echo $TEST_PATH
 
+### START, DISABLE FAST USER SWITCHING ###
 
-touch $TEST_PATH/a_disable_multi_session.sh
+#create a file in the directory
+touch $PATH_TO_BOOT_ONCE/a_disable_multi_session.sh
+#appends whatever is in quotes to the specied path before the >>
 echo '#!/bin/bash' >> $PATH_TO_BOOT_ONCE/a_disable_multi_session.sh
+#appends to a new line. copies whatever is in quotes to the specified path before >>
 echo 'defaults write /Library/Preferences/.GlobalPreferences MultipleSessionEnabled -bool NO' >> $PATH_TO_BOOT_ONCE/a_disable_multi_session.sh
